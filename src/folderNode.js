1b1b9caa885348ce543fe59f201752280ce091f3
Ramon Moraes <ramon.moraes94@gmail.com>
2018-12-28 17:50:57 -0300

    create folderNode class

--- src/folderNode.js
+++ src/folderNode.js
@@ -1,7 +1,21 @@
-module.exports = function(name, isFolder, parent) {
-  return {
-    name,
-    isFolder,
-    parent,
-  };
+module.exports = class FolderNode {
+  constructor(name, isFolder) {
+    this.name = name;
+    this.isFolder = isFolder;
+    this.childrens = [];
+    this.parent = null;
+  }
+
+  generateFolderNode() {
+    const children = new FolderNode();
+
+    this.childrens.push(children);
+    children.parent = this;
+
+    return children;
+  }
+
+  getFullPath() {
+    throw new Exception("To be implemented...");
+  }
 }
