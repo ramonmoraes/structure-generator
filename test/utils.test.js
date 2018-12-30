8cc411fb021924f97aadb80f47a1595469aee7f4
Ramon Moraes <ramon.moraes94@gmail.com>
2018-12-30 12:45:28 -0300

    add isFolder test

--- test/utils.test.js
+++ test/utils.test.js
@@ -7,8 +7,20 @@ describe('Utils', () => {
     it('Count Space should count spaces correctly', () => {
         const twospace = "  twospace";
         const threespace = "   threespace";
-        
+
         expect(countSpacesBefore(twospace)).to.be.equal(2)
         expect(countSpacesBefore(threespace)).to.be.equal(3)
     });
+
+    it('isFolder should reconize a folder pattern', () => {
+        const folder = "folder/";
+        expect(isFolder(folder)).to.be.true;
+
+        const file = "file"
+        expect(isFolder(file)).to.be.false;
+
+        const cleanFolder = "folder";
+        const cleanFolderRegex = /\w+/
+        expect(isFolder(cleanFolder, cleanFolder)).to.be.true;
+    })
 });
