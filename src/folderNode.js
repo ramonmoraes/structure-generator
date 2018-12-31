09dea22c98715f58c07d8e6113efe92834ff96c2
Ramon Moraes <ramon.moraes94@gmail.com>
2018-12-31 16:45:31 -0300

    add getFull path method

--- src/folderNode.js
+++ src/folderNode.js
@@ -3,7 +3,7 @@ module.exports = class FolderNode {
     this.name = options.name;
     this.isFolder = options.isFolder;
     this.spaces = options.spaces;
-    
+
     this.childrens = [];
     this.parent = null;
   }
@@ -13,7 +13,11 @@ module.exports = class FolderNode {
     childrenNode.parent = this;
   }
 
-  getFullPath() {
-    throw new Exception("To be implemented...");
+  getFullPath(parents = []) {
+    parents.push(this.name);
+    if (this.parent == null) {
+      return parents.reverse().join('/');
+    }
+    return this.parent.getFullPath(parents);
   }
 }
