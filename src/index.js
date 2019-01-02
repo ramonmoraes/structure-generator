686541551555d71e9a23730a1ba40f4f38940def
Ramon Moraes <ramon.moraes94@gmail.com>
2019-01-02 13:58:36 -0300

    create CLI mvp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+const fs = require("fs");
+
 const Project = require("./project.js");
+const projectCreator = require("./projectCreator.js");
 
-const template = `
-root/
-  - folder1/
-    - file.txt
-  - anotherFile
-  - folder2/
-    - file  
-  - folder3/
-`;
+const infoMessage = `Find more infos at: https://github.com/ramonmoraes/structure-generator
 
-new Project(template);
+Usage:
+structure-generator 'template-file-path' 'create-location-path'
+`
+
+const args = process.argv.slice(2);
+if (args.length == 0 || args.indexOf("--h") != -1) {
+  console.info(infoMessage)
+} else {
+  const filePath = args[0];
+  const relativePath = args[1];
+
+  const template = fs.readFileSync(filePath, "utf8");
+  const proj = new Project(template, {
+    relativePath
+  });
+
+  projectCreator(proj);
+  console.log(`Project created at '${relativePath}'`);
+}
