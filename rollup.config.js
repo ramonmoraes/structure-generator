cbf8efe073f7188d8b82d8aabbf86aedbcb76554
Ramon Moraes <ramon.moraes94@gmail.com>
2019-01-02 16:19:09 -0300

    add suport to cjs modules to rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,18 @@
+const commonjs = require('rollup-plugin-commonjs');
+const nodeResolve = require('rollup-plugin-node-resolve');
+
 export default {
     input: 'src/index.js',
     output: {
       file: 'bin/structure-generator.js',
       format: 'cjs'
-    }
+    },
+    plugins: [
+      nodeResolve({
+        jsnext: true,
+        main: true
+      }),
+  
+      commonjs()
+    ]
   };
