da345b25f01a8a20d4d0a5b80fad04fe7173f4bb
Ramon Moraes <ramon.moraes94@gmail.com>
2019-01-01 23:33:30 -0300

    add test to project class

---
+++ test/project.test.js
@@ -0,0 +1,23 @@
+const { expect } = require("chai");
+const { describe } = require("mocha");
+
+const Project = require("../src/project.js");
+
+describe.only("Project", () => {
+  it("Should build a project correctly", () => {
+    const template = `
+    root/
+      folder1/
+        - file.txt
+        - cat.gif
+      anotherFile
+      folder2/
+    `;
+
+    const root = new Project(template).root;
+    expect(root.childrens.length).to.be.equal(3);
+    
+    const folder1 = root.childrens[0];
+    expect(folder1.childrens.length).to.be.equal(2);
+  });
+});
