365e154dc6e4be6220e21bb90129099a28cbfb18
Ramon Moraes <ramon.moraes94@gmail.com>
2019-01-02 18:28:23 -0300

    organize function orders

--- src/projectCreator.js
+++ src/projectCreator.js
@@ -1,6 +1,17 @@
 const fs = require("fs");
 const path = require("path");
 
+function projectCreator(project) {
+  const projectPaths = [];
+  appendChildrenPaths(project.root, projectPaths);
+  console.info(projectPaths);
+
+  projectPaths.forEach(projectPath => {
+    projectPath = path.join(process.env.INIT_CWD, projectPath);
+    createCorrectFile(projectPath);
+  });
+}
+
 function appendChildrenPaths(node, arr = []) {
   if (arr.length == 0) {
     arr.push(node.getFullPath());
@@ -11,20 +22,9 @@ function appendChildrenPaths(node, arr = []) {
   });
 }
 
-function projectCreator(project) {
-  const projectPaths = [];
-  appendChildrenPaths(project.root, projectPaths);
-  console.log(projectPaths)
-
-  projectPaths.forEach(projectPath => {
-    projectPath = path.join(process.env.INIT_CWD, projectPath);
-    createCorrectFile(projectPath);
-  });
-}
-
 function createCorrectFile(projectPath) {
   if (projectPath.includes(".")) {
-    fs.writeFileSync(projectPath, '');
+    fs.writeFileSync(projectPath, "");
   } else {
     fs.mkdirSync(projectPath, { recursive: true });
   }
