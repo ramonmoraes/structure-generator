c607d703e146efd1b7ce25ec31c8d7cefca79413
Ramon Moraes <ramon.moraes94@gmail.com>
2019-01-01 22:34:30 -0300

    extract project class to a separate class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-const FolderNode = require("./folderNode.js");
-const { cleanName, countSpacesBefore, isFolder } = require("./utils.js");
+const Project = require("./project.js");
 
 const template = `
 root/
@@ -10,51 +9,5 @@ root/
     - file  
   - folder3/
 `;
-
-class Project {
-  constructor(template = "", options = {}) {
-    this.elements = template.split("\n").filter(str => str != "");
-    this.createProject();
-  }
-
-  getBasicFolderNodes() {
-    return this.elements.map(
-      ele =>
-        new FolderNode({
-          name: cleanName(ele),
-          isFolder: isFolder(ele),
-          spaces: countSpacesBefore(ele)
-        })
-    );
-  }
-
-  createProject() {
-    const nodes = this.getBasicFolderNodes()
-    let root;
-
-    for (let i = 0; i < nodes.length; i++) {
-      const node = nodes[i];
-      if (i == 0) root = node;
-
-      if (i != nodes.length - 1) {
-        const nextNode = nodes[i + 1];
-        this.appendNodes(node, nextNode);   
-      }
-    }
-    
-    return root;
-  }
-
-  appendNodes(node, nextNode) {
-    if (nextNode.spaces > node.spaces) {
-      node.addChildren(nextNode);
-    } else if (node.parent && nextNode.spaces == node.spaces) {
-      node.parent.addChildren(nextNode);
-    } else if (node.parent) {
-      this.appendNodes(node.parent, nextNode);
-    }
-  }
-
-}
 
 new Project(template);
---
+++ src/project.js
@@ -0,0 +1,47 @@
+const { cleanName, countSpacesBefore, isFolder } = require("./utils.js");
+const FolderNode = require("./folderNode.js");
+
+module.exports = class Project {
+  constructor(template = "", options = {}) {
+    this.elements = template.split("\n").filter(str => str != "");
+    this.createProject();
+  }
+
+  getBasicFolderNodes() {
+    return this.elements.map(
+      ele =>
+        new FolderNode({
+          name: cleanName(ele),
+          isFolder: isFolder(ele),
+          spaces: countSpacesBefore(ele)
+        })
+    );
+  }
+
+  createProject() {
+    const nodes = this.getBasicFolderNodes();
+    let root;
+
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i];
+      if (i == 0) root = node;
+
+      if (i != nodes.length - 1) {
+        const nextNode = nodes[i + 1];
+        this.appendNodes(node, nextNode);
+      }
+    }
+
+    return root;
+  }
+
+  appendNodes(node, nextNode) {
+    if (nextNode.spaces > node.spaces) {
+      node.addChildren(nextNode);
+    } else if (node.parent && nextNode.spaces == node.spaces) {
+      node.parent.addChildren(nextNode);
+    } else if (node.parent) {
+      this.appendNodes(node.parent, nextNode);
+    }
+  }
+};
